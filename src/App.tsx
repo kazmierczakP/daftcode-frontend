7abc697f89c5702c7e1fecadad86d256fd0d1d0e
Patryk <patryklcj@gmail.com>
2022-05-22 18:29:16 +0200

    feat(form): add form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { InputForm } from './InputForm';
 
 function App() {
   const handleShowAlert = () => {
@@ -9,11 +10,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <InputForm defaultValue='system ready'></InputForm>
+
+        {/*}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <button onClick={handleShowAlert}>Show alert</button>
+        */}
         <a
           className="App-link"
           href="https://github.com/xkk-opps/daftcode-frontend"
@@ -21,14 +26,6 @@ function App() {
           rel="noopener noreferrer"
         >
           RepoLink
-        </a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
         </a>
       </header>
     </div>
---
+++ src/InputForm/InputForm.tsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react"
+
+interface InputFormProps {
+    defaultValue?: string
+}
+
+const storageFromKey = 'inputValue'
+
+export const InputForm = (props?: InputFormProps) => {
+    const [displayError, setDisplayError] = useState<boolean>(false)
+    const [inputValue, setInputValue] = useState<string>('')
+    const [errorMessage, setErrorMessage] = useState<string>('')
+
+    const formReadyToSubmit = !displayError || inputValue
+
+    const handleInputChange= (event: any) => {
+        const messageLength = event.target.value.length
+        setInputValue(event.target.value)
+        if (messageLength < 3 || messageLength >20) {
+            if (messageLength < 3) {
+                setErrorMessage('User name is too short')
+            }
+            if (messageLength > 20) {
+                setErrorMessage('User name is too long')
+            }
+            setDisplayError(true)
+        } else {
+            setDisplayError(false)
+            setErrorMessage('')
+        }
+    }
+
+    const handleSubmit = () => {
+        if (formReadyToSubmit) {
+            localStorage.setItem(storageFromKey, inputValue)
+        }
+    }
+
+    useEffect(() => {
+        let valueToSet =''
+        const localStorageData = localStorage.getItem(storageFromKey)
+        if (!!localStorageData) {
+            valueToSet = localStorageData
+        } else {
+            if (!!props?.defaultValue) {
+                valueToSet = props?.defaultValue
+            }
+        }
+        setInputValue(valueToSet)
+    }, [])
+
+    return <div style={{display: 'flex', flexDirection: 'column', flexWrap: 'wrap'}}>
+        {displayError && <div style={{color: 'red'}}>{errorMessage}</div>}
+        <div>User name:</div>
+        <input onInput={handleInputChange} value={inputValue} type='text'></input>
+        <button onClick={handleSubmit} disabled={!formReadyToSubmit}>submit data</button>
+        <br></br>
+    </div>
+}
