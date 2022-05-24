37e53e80aaa483b3397d813d459d95ae5299aa9b
Patryk <patryklcj@gmail.com>
2022-05-25 00:18:09 +0200

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { InputForm } from './InputForm';
 
 function App() {
-  const handleShowAlert = () => {
+/*const handleShowAlert = () => {
     alert("Hello react")
-  }
+  }*/
   return (
     <div className="App">
       <header className="App-header">
         <InputForm defaultValue='system ready'></InputForm>
-
-        {/*}
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <button onClick={handleShowAlert}>Show alert</button>
-        */}
         <a
           className="App-link"
           href="https://github.com/xkk-opps/daftcode-frontend"
--- src/InputForm/InputForm.tsx
+++ src/InputForm/InputForm.tsx
@@ -47,7 +47,7 @@ export const InputForm = (props?: InputFormProps) => {
             }
         }
         setInputValue(valueToSet)
-    }, [])
+    }, [props])
 
     return <div style={{display: 'flex', flexDirection: 'column', flexWrap: 'wrap'}}>
         {displayError && <div style={{color: 'red'}}>{errorMessage}</div>}
