57832938371012b16bc8b166afd15b8dd4b1fc27
Patryk <patryklcj@gmail.com>
2022-06-05 21:25:39 +0200

    new branch init

--- src/InputForm/InputForm.tsx
+++ src/InputForm/InputForm.tsx
@@ -1,10 +1,32 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect} from "react"
+import Airtable from "airtable"
 
 interface InputFormProps {
     defaultValue?: string
 }
 
 const storageFromKey = 'inputValue'
+
+interface RMRResponseItem {
+    created: string;
+    episode: Array<string>
+    gender: string
+    id: number | string
+    image: string;
+    location: {name: string, url: string}
+    name: string
+    origin: {name: string, url: string}
+    species: string
+    status: string
+    type: string
+    url: string
+}
+
+const apiKey = process.env.REACT_APP_AIRTABLE_API_KEY
+const dbId = process.env.REACT_APP_AIRTABLE_API_TEST_DB_ID
+const tableName = 'Table 1'
+//const base = new Airtable({apiKey}).base('%dbId');
+const base = new Airtable({apiKey: 'keyt9ug9p4HIYgoqR'}).base('appyMA6TtwNWi64eq');
 
 export const InputForm = (props?: InputFormProps) => {
     const [displayError, setDisplayError] = useState<boolean>(false)
@@ -32,7 +54,7 @@ export const InputForm = (props?: InputFormProps) => {
 
     const handleSubmit = () => {
         if (formReadyToSubmit) {
-            localStorage.setItem(storageFromKey, inputValue)
+            handleSetNewRecord();
         }
     }
 
@@ -48,6 +70,40 @@ export const InputForm = (props?: InputFormProps) => {
         }
         setInputValue(valueToSet)
     }, [props])
+
+    const handleSetNewRecord = () => {
+        base(tableName).create([
+            {
+                fields: {
+                    Name: inputValue
+                }
+            },
+        ], function (TableError, records: any){
+            if (TableError){
+                console.error(TableError);
+                return;
+            }
+            // records.forEach(function (record: any){
+            //     console.log(record);
+            // });
+            alert('Value saved in db, value:${records[0].Name}')
+            setInputValue('')
+        });
+    }
+
+    const handleAirtable = () => {
+        base(tableName)
+        .select({})
+        .eachPage(function page(records, fetchNextPage) {
+            records.forEach(function(record) {
+                console.log('Retreived', record.get('Name'));
+            });
+            fetchNextPage();
+        }, function done(CallbackError) {
+            if (CallbackError) {console.error(CallbackError); return;}
+        });
+    };
+    //handleAirtable();
 
     return <div style={{display: 'flex', flexDirection: 'column', flexWrap: 'wrap'}}>
         {displayError && <div style={{color: 'red'}}>{errorMessage}</div>}
